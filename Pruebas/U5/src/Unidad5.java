4b77ee18c8f9379a4963d9c2ecb0f12fda02bcf1
Agustin Silva <agstnsilva@outlook.com>
2023-07-20 22:18:10 -0400

    Menu Principal listo, inciio menu clase crear clase

--- Pruebas/U5/src/Unidad5.java
+++ Pruebas/U5/src/Unidad5.java
@@ -1,16 +1,87 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Unidad5 {
 
 	public static void main(String[] args) {
 		
+		Scanner sc = new Scanner(System.in);
 		ArrayList<Clase> clases = new ArrayList<Clase>();
 		Clase clas;		
 		Variable variable;
 		Metodo metodo;
 		
-		EjemploBase();
-				
+		int menuPrincipal, menuclase, menuvaraible, menumetodo;
+		menuPrincipal = 0;
+		System.out.println("Bienvenido");
+		do {
+			System.out.println(MenuPrincipal());
+			menuPrincipal = sc.nextInt();
+			switch(menuPrincipal) {
+			case 1:
+				System.out.println("\n=======================================\n");
+				EjemploBase();
+				System.out.println("\n=======================================\n");
+				break;
+			case 2:
+				menuclase = 0;
+				do {
+				System.out.println("\n=======================================\n");
+				if(clases != null) {
+					System.out.println(MenuClases(true));
+				}else {
+					System.out.println(MenuClases(false));
+				}
+				menuclase = sc.nextInt();
+				switch(menuclase) {
+				case 1:
+						String NomClas = "";
+						do {
+						
+						}while(NomClas != "");
+						
+						String AccClas = "";
+						do {
+							
+						}while(AccClas != "");
+						
+						
+						
+					break;
+				case 2:
+					System.out.println("____________________________________\n");
+					for(int i = 0; i<clases.size();i++) {
+						System.out.println(i+". "+clases.get(i).Acceso+"\n");
+					}
+					System.out.println("____________________________________\n");
+					break;
+				case 3:
+					break;
+				case 4:
+					break;
+				case 5:
+					break;
+				default:
+					System.out.println("Debe seleccionar una opcion valida");
+					menuclase = 0;
+				}
+				System.out.println("\n=======================================\n");
+				}while(menuclase!=5);
+				break;
+			case 3:
+				break;
+			case 4:
+				break;
+			case 5:
+				break;
+			case 6:
+				break;
+			default:
+				System.out.println("Debe seleccionar una opcion valida");
+				menuPrincipal = 0;
+			}
+		
+		}while(menuPrincipal != 6);	
 		
 	}
 	
@@ -38,6 +109,34 @@ public class Unidad5 {
 		System.out.println(clasesEjem.get(0).ListaConstructor());
 		System.out.println("\n");
 		System.out.println(clasesEjem.get(0).ListStringMetodos()+"\n}");
+	}
+	
+	public static String MenuPrincipal() {
+		String retorno = "";
+		retorno += "Menu Principal \n";
+		retorno +="Seleccione una opcion \n";
+		retorno +="1. Ejecutar ejemplo de clase formato standar java \n";
+		retorno +="2. Agregar o modificar clase \n";
+		retorno +="3. Agregar  o modificar Variables de una clase \n";
+		retorno +="4. Agregar o modificar Metodos de una Clase \n";
+		retorno +="5. Listar Clase en formato \n";
+		retorno +="6. Salir \n";
+		return retorno;
+	}
+	
+	public static String MenuClases(boolean ExisteClase) {
+		String retorno = "";
+		retorno += "Menu Clases \n";
+		retorno +="Seleccione una opcion \n";
+		retorno +="1. Agregar Nueva Clase \n";
+		if(ExisteClase) {
+			retorno +="2. Listar Clases \n";
+			retorno +="3. Modificar una Clase \n";
+			retorno +="4. Eliminar una Clase \n";
+		}
+		retorno +="5. Volver al Menu Principal \n";
+		return retorno;
+		
 	}
 
 }
